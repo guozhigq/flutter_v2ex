610a2abb856226e9883597b8d0b5bde44b323e33
guozhigq <guozhigq@outlook.com>
2023-03-08 22:07:58 +0800

    fix: 回复文本输入框高度自适应

--- lib/components/topic/member_list.dart
+++ lib/components/topic/member_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_v2ex/components/common/avatar.dart';
 import 'package:flutter_v2ex/utils/storage.dart';
+import 'package:get/get.dart';
 
 class ReplyMemberList extends StatefulWidget {
   List? replyList;
@@ -13,59 +14,129 @@ class ReplyMemberList extends StatefulWidget {
 
 class _ReplyMemberListState extends State<ReplyMemberList> {
   final statusBarHeight = GStorage().getStatusBarHeight();
+  final ScrollController _listScrollController = ScrollController();
+
+  // 滑动至顶部下拉关闭bottomSheet +2 降低灵敏度
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification is ScrollEndNotification &&
+        _listScrollController.offset + 2 <= _listScrollController.position.minScrollExtent &&
+        notification.metrics.extentBefore == 0) {
+      Get.back();
+      return true;
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height - statusBarHeight - 10,
-        padding: const EdgeInsets.only(top: 5),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 40,
-              padding: const EdgeInsets.only(bottom: 2),
-              child: Center(
-                child: Container(
-                  width: 38,
-                  height: 2,
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(3),
-                      ),
-                      color: Theme.of(context).colorScheme.onSurface),
-                ),
+      clipBehavior: Clip.hardEdge,
+      height: MediaQuery.of(context).size.height - statusBarHeight - 40,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      ),
+      // child: Column(
+      //   children: [
+      //     Container(
+      //       padding: const EdgeInsets.only(top: 10, left: 20, right: 15),
+      //       margin: const EdgeInsets.only(bottom: 5),
+      //       child: sheetHead(),
+      //     ),
+      //     Expanded(
+      //       child: ListView.builder(
+      //         padding: EdgeInsets.zero,
+      //         itemCount: widget.replyList!.length,
+      //         itemBuilder: (BuildContext context, int index) {
+      //           if (index == widget.replyList!.length) {
+      //             return SizedBox(
+      //                 height: MediaQuery.of(context).padding.bottom);
+      //           } else {
+      //             return memberItem(widget.replyList![index]);
+      //           }
+      //         },
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: null,
+          automaticallyImplyLeading: false,
+          toolbarHeight: 65.0,
+          title: sheetHead(),
+        ),
+        body: NotificationListener<ScrollNotification>(
+          onNotification: _handleScrollNotification,
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            controller: _listScrollController,
+            itemCount: widget.replyList!.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == widget.replyList!.length) {
+                return SizedBox(height: MediaQuery.of(context).padding.bottom);
+              } else {
+                return memberItem(widget.replyList![index]);
+              }
+            },
+          ),
+      )
+        ,
+      ),
+    );
+  }
+
+  Widget sheetHead() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text.rich(
+          TextSpan(
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(letterSpacing: 1),
+            children: [
+              const TextSpan(text: '选择要'),
+              TextSpan(
+                text: '@',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(fontWeight: FontWeight.w900),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
-                itemCount: widget.replyList!.length,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (BuildContext context, int index) {
-                  return memberItem(widget.replyList![index]);
-                },
-              ),
-            ),
-          ],
-        ));
+              const TextSpan(text: '的用户')
+            ],
+          ),
+        ),
+        IconButton(
+          tooltip: '确认并收起',
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_downward_rounded),
+          style: IconButton.styleFrom(
+            padding: const EdgeInsets.all(9),
+            // backgroundColor: Theme.of(context).colorScheme.background
+          ),
+        ),
+      ],
+    );
   }
 
   Widget memberItem(replyItem) {
     return ListTile(
       onTap: () {
         // 直接选中
-        Navigator.pop(context, {
-          'atMemberList': List.filled(1, replyItem)
-        });
+        Navigator.pop(context, {'atMemberList': List.filled(1, replyItem)});
       },
-      leading: CAvatar(url: replyItem.avatar, size: 42, quality: 'origin'),
+      leading: CAvatar(url: replyItem.avatar, size: 38, quality: 'origin'),
       title: Text.rich(TextSpan(children: [
+        TextSpan(text: '${replyItem.userName} '),
         TextSpan(
-          text: '${replyItem.userName} ',
-        ),
-        TextSpan(
-            text: ' #${replyItem.floorNumber}',
+            text: ' ${replyItem.floorNumber}楼',
             style: Theme.of(context)
                 .textTheme
                 .labelSmall!
@@ -74,7 +145,7 @@ class _ReplyMemberListState extends State<ReplyMemberList> {
       subtitle: Text(replyItem.content,
           maxLines: 1, style: Theme.of(context).textTheme.labelMedium!),
       trailing: Transform.scale(
-        scale: 1.1,
+        scale: 0.8,
         child: Checkbox(
           value: replyItem.isChoose,
           onChanged: (bool? checkValue) {
