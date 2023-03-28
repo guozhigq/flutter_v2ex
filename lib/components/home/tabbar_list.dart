500718115263669f405550bc6176cced5e92ece3
guozhigq <guozhigq@outlook.com>
2023-03-28 14:16:10 +0800

    mod: 列表滚动惯性取消

--- lib/components/home/tabbar_list.dart
+++ lib/components/home/tabbar_list.dart
@@ -183,7 +183,9 @@ class _TabBarListState extends State<TabBarList>
               child: ListView.builder(
                 padding: const EdgeInsets.only(top: 1, bottom: 0),
                 physics: const AlwaysScrollableScrollPhysics(
-                    parent: BouncingScrollPhysics()),
+                    // parent: BouncingScrollPhysics(), // iOS
+                  parent: ClampingScrollPhysics() // Android
+                ),
                 //重要
                 itemCount: topicList.length + 1,
                 controller: _controller,
