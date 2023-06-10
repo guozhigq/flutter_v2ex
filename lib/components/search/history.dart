abe342e54e8546c02f092ec4ccbd7a80aaca8553
guozhigq <guozhigq@outlook.com>
2023-06-10 23:38:52 +0800

    fix: 搜索历史清空

--- lib/components/search/history.dart
+++ lib/components/search/history.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_v2ex/service/search.dart';
 
-class SearchHistory extends StatelessWidget {
+class SearchHistory extends StatefulWidget {
   List? searchHisList;
   Function? onSelect;
   Function? onClear;
   SearchHistory({super.key, this.searchHisList, this.onSelect, this.onClear});
 
+  @override
+  State<SearchHistory> createState() => _SearchHistoryState();
+}
+
+class _SearchHistoryState extends State<SearchHistory> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,67 +20,77 @@ class SearchHistory extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: searchHisList != null && searchHisList!.isNotEmpty
-            ? [
-                Row(
-                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text('搜索历史'),
-                    const Spacer(),
-                    TextButton(
-                        // onPressed: () {
-                        //   Search().clear();
-                        //   onClear!();
-                        // },
-                        onPressed: () => showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: const Text("提示"),
-                                content: const Text("您确定要删除搜索历史吗?"),
-                                actions: <Widget>[
-                                  TextButton(
-                                    child: Text("取消", style: TextStyle(color: Theme.of(context).colorScheme.outline),),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                  TextButton(
-                                    child: const Text("确定"),
-                                    onPressed: () {
-                                      Search().clear();
-                                      onClear!();
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                ],
-                              );
-                            }),
-                        child: Text(
-                          '清空',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.outline),
-                        )),
-                    const SizedBox(width: 10)
-                  ],
-                ),
-                const SizedBox(height: 6),
-                Wrap(
-                  spacing: 8,
-                  runSpacing: 8,
-                  direction: Axis.horizontal,
-                  textDirection: TextDirection.ltr,
-                  children: [
-                    for (int i = 0; i < searchHisList!.length; i++)
-                      SearchText(
-                        searchText: searchHisList![i],
-                        searchTextIdx: i,
-                        onSelect: onSelect,
-                      )
-                  ],
-                )
-              ]
-            : [],
+        children:
+            widget.searchHisList != null && widget.searchHisList!.isNotEmpty
+                ? [
+                    Row(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('搜索历史'),
+                        const Spacer(),
+                        TextButton(
+                            // onPressed: () {
+                            //   Search().clear();
+                            //   onClear!();
+                            // },
+                            onPressed: () => showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text("提示"),
+                                    content: const Text("您确定要删除搜索历史吗?"),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text(
+                                          "取消",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .outline),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      TextButton(
+                                        child: const Text("确定"),
+                                        onPressed: () {
+                                          Search().clear();
+                                          widget.onClear!();
+                                          setState(() {
+                                            widget.searchHisList = [];
+                                          });
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                }),
+                            child: Text(
+                              '清空',
+                              style: TextStyle(
+                                  color: Theme.of(context).colorScheme.outline),
+                            )),
+                        const SizedBox(width: 10)
+                      ],
+                    ),
+                    const SizedBox(height: 6),
+                    Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      direction: Axis.horizontal,
+                      textDirection: TextDirection.ltr,
+                      children: [
+                        for (int i = 0; i < widget.searchHisList!.length; i++)
+                          SearchText(
+                            searchText: widget.searchHisList![i],
+                            searchTextIdx: i,
+                            onSelect: widget.onSelect,
+                          )
+                      ],
+                    )
+                  ]
+                : [],
       ),
     );
   }
