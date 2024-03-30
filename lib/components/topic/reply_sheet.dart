407093813c5923d30b14a8ae6d92346065b976fb
guozhigq <guozhigq@outlook.com>
2024-03-30 15:12:55 +0800

    fix: 查看回复异常 issues #92

--- lib/components/topic/reply_sheet.dart
+++ lib/components/topic/reply_sheet.dart
@@ -115,22 +115,29 @@ class _ReplySheetState extends State<ReplySheet> with TickerProviderStateMixin {
                     var i = widget.replyMemberList.indexOf(e);
                     print('104: ${widget.resultList[i]}');
                     // return Text(e);
-                    return ListView.builder(
-                      physics: const ClampingScrollPhysics(), //重要
-                      itemCount: widget.resultList[i][e].length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                          child: ReplyListItem(
-                            reply: widget.resultList[i][e][index],
-                            topicId: widget.topicId,
-                            totalPage: widget.totalPage,
-                            source: 'sheet',
-                          ),
-                        );
-                        // return Text('123');
-                      },
-                    );
+                    return widget.resultList[i][e] != null
+                        ? ListView.builder(
+                            physics: const ClampingScrollPhysics(), //重要
+                            itemCount: widget.resultList[i][e]?.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                                child: ReplyListItem(
+                                  reply: widget.resultList[i][e][index],
+                                  topicId: widget.topicId,
+                                  totalPage: widget.totalPage,
+                                  source: 'sheet',
+                                ),
+                              );
+                              // return Text('123');
+                            },
+                          )
+                        : const SizedBox(
+                            child: Center(
+                              child: Text('无回复内容'),
+                            ),
+                          );
                   }).toList(),
                 ),
               ),
